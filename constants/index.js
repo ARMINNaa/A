bf4ba9c7612b6866c5a379f36d1ccb7b20c7983f
ARMINNaa <74266632+ARMINNaa@users.noreply.github.com>
2023-07-20 00:38:11 -0700

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x90803fe4b29a23c464d768d863c2260ac5f6141b";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
